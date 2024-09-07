57faaaae15e0fe7471cc8cb7226d4a9b057f4626
Asta86 <psljp@protonmail.com>
2024-09-07 13:28:04 -0300

    Add Copy-to-clipboard buttons

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,30 @@
-'use strict';
+"use strict";
 
 // Set Copyright year in the Footer
-document.getElementById('yearSpan').textContent = new Date().getFullYear();
+document.getElementById("yearSpan").textContent = new Date().getFullYear();
 
-document.getElementById('userCpBtn')?.addEventListener('click', function () {
-    const textToCopy = document.getElementById('cpTxtA').textContent;
-    
-    navigator.clipboard.writeText(textToCopy).then(function() {
-        const tooltip = document.getElementById('cpttip');
-        // Show the tooltip when the button is clicked
-        tooltip.style.visibility = 'visible';
-        // Hide the tooltip when not hovering the button
-        document.getElementById('userCpBtn').addEventListener('mouseleave', function () {
-            tooltip.style.visibility = 'hidden';
-        });
-    })
-});
+// Copy-to-clipboard buttons
+const idls = [
+  ["userCpBtn", "cpTxtA", "cpTtipA"],
+  ["addrICpBtn", "cpTxtB", "cpTtipB"],
+  ["addrIICpBtn", "cpTxtC", "cpTtipC"],
+];
+
+for (let idl of idls) {
+  const btn = document.getElementById(idl[0]);
+  if (btn === null) continue;
+
+  btn.addEventListener("click", function () {
+    const textToCopy = document.getElementById(idl[1]).textContent;
+
+    navigator.clipboard.writeText(textToCopy).then(function () {
+      const tooltip = document.getElementById(idl[2]);
+      // Show the tooltip when the button is clicked
+      tooltip.style.visibility = "visible";
+      // Hide the tooltip when not hovering the button
+      btn.addEventListener("mouseleave", function () {
+        tooltip.style.visibility = "hidden";
+      });
+    });
+  });
+}
